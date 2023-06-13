0a5495f6d895223e0bf09624d0afb02175b13ac0
(Arta) Mohammadreza Javadikouchaksarei <a.javadikouchaksarei@reply.de>
2023-06-13 22:18:31 +0200

    refactor: rename spring boot application

--- src/main/java/de/tudortmund/wt2/luminary/LuminaryApplication.java
+++ src/main/java/de/tudortmund/wt2/luminary/LuminaryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class LuminaryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(LuminaryApplication.class, args);
 	}
 
 }
